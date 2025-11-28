8961fa860435c25e8097455f45d9cd0813353810
Henri-John <henri-john@Henri-Johns-MacBook-Air.local>
2025-11-28 17:36:21 +0200

    Fix mobile responsiveness for TitleBar component

--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -5,11 +5,11 @@ const TitleBar: React.FC = () => {
     return (
         <div className="h-9 bg-[#3c3c3c] flex items-center justify-between px-2 text-xs select-none border-b border-black/20">
             {/* Left: Menu items */}
-            <div className="flex items-center gap-1">
+            <div className="flex items-center gap-1 flex-shrink-0">
                 <div className="px-2 py-1 hover:bg-white/10 cursor-pointer rounded">
                     <img src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3E%3Cpath fill='%2300a8e8' d='M8 0L0 8l8 8 8-8z'/%3E%3C/svg%3E" alt="VS Code" className="w-4 h-4" />
                 </div>
-                <div className="flex items-center text-[#cccccc]">
+                <div className="hidden md:flex items-center text-[#cccccc]">
                     <span className="px-2 py-1 hover:bg-white/10 cursor-pointer">File</span>
                     <span className="px-2 py-1 hover:bg-white/10 cursor-pointer">Edit</span>
                     <span className="px-2 py-1 hover:bg-white/10 cursor-pointer">Selection</span>
@@ -22,16 +22,17 @@ const TitleBar: React.FC = () => {
             </div>
 
             {/* Center: Title */}
-            <div className="flex-1 text-center text-[#cccccc] font-normal text-xs">
-                Henri-John Plaatjies - Senior Test Automation Engineer
+            <div className="flex-1 text-center text-[#cccccc] font-normal text-xs px-2 overflow-hidden">
+                <span className="hidden sm:inline">Henri-John Plaatjies - Senior Test Automation Engineer</span>
+                <span className="sm:hidden truncate block">Henri-John Plaatjies</span>
             </div>
 
             {/* Right: Window controls */}
-            <div className="flex items-center">
-                <div className="p-2 hover:bg-white/10 cursor-pointer text-white">
+            <div className="flex items-center flex-shrink-0">
+                <div className="p-2 hover:bg-white/10 cursor-pointer text-white hidden sm:block">
                     <Minus size={14} />
                 </div>
-                <div className="p-2 hover:bg-white/10 cursor-pointer text-white">
+                <div className="p-2 hover:bg-white/10 cursor-pointer text-white hidden sm:block">
                     <Square size={12} />
                 </div>
                 <div className="p-2 hover:bg-red-600 cursor-pointer text-white">
